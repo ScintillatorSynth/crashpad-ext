3066c9ff46cfd58b1721433b34be827cfd3d4d3f
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-24 16:16:05 -0700

    try script to find the windows sdk

--- .travis/script-windows.sh
+++ .travis/script-windows.sh
@@ -4,3 +4,4 @@ cmd < $TRAVIS_BUILD_DIR/script-windows.bat
 
 mkdir $TRAVIS_BUILD_DIR/install-ext
 cp -R $TRAVIS_BUILD_DIR/crashpad/crashpad $TRAVIS_BUILD_DIR/install-ext/.
+
--- script-windows.bat
+++ script-windows.bat
@@ -1,7 +1,16 @@
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 set PATH=%TRAVIS_HOME%\depot_tools;c:\go\bin;%PATH%
 
+echo "---- finding windows sdk"
+%TRAVIS_BUILD_DIR%\find-windows-sdk.bat
+
 echo "---- NINJA CRASHPAD"
+cd %TRAVIS_BUILD_DIR%\crashpad\crashpad
+mkdir out
+cd out
+mkdir Default
+cd Default
+echo win_toolchain_path="%WindowsSdkPath%" > args.gn
 cd %TRAVIS_BUILD_DIR%\crashpad\crashpad
 gn gen out\Default
 ninja -C out\Default
