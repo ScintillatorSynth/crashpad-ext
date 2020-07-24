9ac0be2386367edbbcfe870c613610b3087646bc
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-23 23:09:34 -0700

    call gn first

--- script-windows.bat
+++ script-windows.bat
@@ -3,6 +3,7 @@ set PATH=%TRAVIS_HOME%\depot_tools;%PATH%
 
 echo "---- NINJA CRASHPAD"
 cd %TRAVIS_BUILD_DIR%\crashpad
+gn gen out\Default
 ninja -C out\Default
 
 echo "---- BUILD BREAKPAD DUMP_SYMS"
