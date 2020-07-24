575507a6f140ae0f721232a95c15823e5d077b19
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-23 22:25:08 -0700

    try boringssl config in batch file

--- .travis/before-script-windows.sh
+++ .travis/before-script-windows.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-# Ninja in depot_tools seems broken, remove it
-rm $TRAVIS_HOME/depot_tools/ninja
-
 $TRAVIS_BUILD_DIR/before-script-windows.bat
-
-cd $TRAVIS_BUILD_DIR
-git clone https://github.com/google/boringssl
-cd boringssl
-mkdir build
-cd build
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release ..
-ninja
 
--- before-script-windows.bat
+++ before-script-windows.bat
@@ -11,3 +11,11 @@ mkdir breakpad
 cd breakpad
 fetch breakpad
 
+cd %TRAVIS_BUILD_DIR%
+git clone https://github.com/google/boringssl
+cd boringssl
+mkdir build
+cd build
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release ..
+ninja
+
