67cfdc04ae87d0641618bef46db4b40496ed3ebf
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-24 09:55:19 -0700

    add go to path

--- .travis/before-script-windows.sh
+++ .travis/before-script-windows.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-cd $TRAVIS_BUILD_DIR
-
 echo "---- starting before-script"
 cmd < $TRAVIS_BUILD_DIR/before-script-windows.bat
 echo "---- before-script exit"
 
-echo "----- BUILD BORINGSSL"
-cd $TRAVIS_BUILD_DIR
-git clone https://github.com/google/boringssl
-
-cd $TRAVIS_BUILD_DIR/boringssl
-mkdir build
-cd build
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release ..
-ninja
-
-echo "*** cmakelog:"
-cat CMakeFiles/CMakeOutput.log
 find $TRAVIS_BUILD_DIR/boringssl -name "*.lib" -print
 
--- script-windows.bat
+++ script-windows.bat
@@ -1,5 +1,5 @@
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
-set PATH=%TRAVIS_HOME%\depot_tools;%PATH%
+set PATH=%TRAVIS_HOME%\depot_tools;c:\go\bin;%PATH%
 
 echo "---- NINJA CRASHPAD"
 cd %TRAVIS_BUILD_DIR%\crashpad\crashpad
