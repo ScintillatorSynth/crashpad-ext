1b13fb1878437e7ba54fafdd77b67a68475cf868
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-23 22:46:30 -0700

    maybe travis will catch up

--- .travis/script-windows.sh
+++ .travis/script-windows.sh
@@ -11,3 +11,5 @@ cat out/Default/args.gn
 
 $TRAVIS_BUILD_DIR/script-windows.bat
 
+mkdir $TRAVIS_BUILD_DIR/install-ext
+cp -R $TRAVIS_BUILD_DIR/crashpad/crashpad $TRAVIS_BUILD_DIR/install-ext/.
--- script-windows.bat
+++ script-windows.bat
@@ -4,3 +4,6 @@ set PATH=%TRAVIS_HOME%\depot_tools;%PATH%
 cd %TRAVIS_BUILD_DIR%\crashpad
 ninja -C out\Default
 
+cd %TRAVIS_BUILD_DIR%\breakpad\src\src\tools\windows\dump_syms
+msbuild dump_syms.vcproj
+
