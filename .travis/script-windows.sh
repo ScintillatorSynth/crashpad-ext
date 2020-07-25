7686797e9dd6c3af260f5ea2185a7f26ef8a13d7
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-25 16:17:12 -0700

    also copy msdia dll so dump_symbols will work

--- .travis/script-windows.sh
+++ .travis/script-windows.sh
@@ -15,4 +15,5 @@ mkdir $TRAVIS_BUILD_DIR/install-ext
 cp -R $TRAVIS_BUILD_DIR/crashpad/crashpad $TRAVIS_BUILD_DIR/install-ext/.
 mkdir $TRAVIS_BUILD_DIR/install-ext/bin
 cp $TRAVIS_BUILD_DIR/breakpad/src/src/tools/windows/binaries/dump_syms.exe $TRAVIS_BUILD_DIR/install-ext/bin/.
+cp $TRAVIS_BUILD_DIR/bin/* $TRAVIS_BUILD_DIR/install-ext/bin/.
 
