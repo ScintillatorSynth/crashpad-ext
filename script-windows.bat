0161a7f5ad4a44c220997379c9547f31a4f30139
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-24 22:20:24 -0700

    try args on command line

--- script-windows.bat
+++ script-windows.bat
@@ -3,6 +3,6 @@ set PATH=%TRAVIS_HOME%\depot_tools;c:\go\bin;%PATH%
 
 echo "---- NINJA CRASHPAD"
 cd %TRAVIS_BUILD_DIR%\crashpad\crashpad
-gn gen out\Default
+gn gen out\Default --args='target_cpu="x64" crashpad_use_boringssl_for_http_transport_socket=true'
 ninja -C out\Default
 
