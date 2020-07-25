3bb39ddd250652071d8631ec6afce260ff8a5f27
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-24 21:43:23 -0700

    might be windows line endings, try bash

--- .travis/script-windows.sh
+++ .travis/script-windows.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+export TRAVIS_WIN_DIR=`cygpath -w $TRAVIS_BUILD_DIR`
+cd $TRAVIS_BUILD_DIR/crashpad/crashpad
+mkdir -p out/Default
+echo "target_cpu=\"x64\"" > out/Default/args.gn
+echo "crashpad_use_boringssl_for_http_transport_socket=true" >> out/Default/args.gn
+echo "extra_clags=\"/I$TRAVIS_WIN_DIR\\boringssl\"" >> args.gn
+echo "extra_ldflags=\"$TRAVIS_WIN_DIR\\build\\ssl\\Release\\ssl.lib $TRAVIS_BUILD_DIR\\build\\crypto\\Release\\crypto.lib\"" >> args.gn
+echo "args.gn:"
+cat args.gn
+
 cmd < $TRAVIS_BUILD_DIR/script-windows.bat
 
 mkdir $TRAVIS_BUILD_DIR/install-ext
 cp -R $TRAVIS_BUILD_DIR/crashpad/crashpad $TRAVIS_BUILD_DIR/install-ext/.
 mkdir $TRAVIS_BUILD_DIR/install-ext/bin
 cp $TRAVIS_BUILD_DIR/breakpad/src/src/tools/windows/binaries/dump_syms.exe $TRAVIS_BUILD_DIR/instal-ext/bin/.
+
--- script-windows.bat
+++ script-windows.bat
@@ -3,15 +3,6 @@ set PATH=%TRAVIS_HOME%\depot_tools;c:\go\bin;%PATH%
 
 echo "---- NINJA CRASHPAD"
 cd %TRAVIS_BUILD_DIR%\crashpad\crashpad
-mkdir out
-cd out
-mkdir Default
-cd Default
-echo target_cpu="x64" > args.gn
-echo crashpad_use_boringssl_for_http_transport_socket=true >> args.gn
-echo "args.gn:"
-type args.gn
-cd %TRAVIS_BUILD_DIR%\crashpad\crashpad
 gn gen out\Default
 ninja -C out\Default
 
