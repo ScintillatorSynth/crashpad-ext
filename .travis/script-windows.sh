a1ea4a6898a7bc1e610fa24059f83c809ad846cd
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-24 22:38:28 -0700

    try skipping boringssl arg

--- .travis/script-windows.sh
+++ .travis/script-windows.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 export TRAVIS_WIN_DIR=`cygpath -w $TRAVIS_BUILD_DIR`
-#cd $TRAVIS_BUILD_DIR/crashpad/crashpad
-#mkdir -p out/Default
-#echo "target_cpu=\"x64\"" > out/Default/args.gn
+cd $TRAVIS_BUILD_DIR/crashpad/crashpad
+mkdir -p out/Default
+echo "target_cpu=\"x64\"" > out/Default/args.gn
 #echo "crashpad_use_boringssl_for_http_transport_socket=true" >> out/Default/args.gn
-#echo "extra_cflags=\"/I$TRAVIS_WIN_DIR\\boringssl\"" >> out/Default/args.gn
-#echo "extra_ldflags=\"$TRAVIS_WIN_DIR\\build\\ssl\\Release\\ssl.lib $TRAVIS_WIN_DIR\\build\\crypto\\Release\\crypto.lib\"" >> out/Default/args.gn
-#echo "args.gn:"
-#cat out/Default/args.gn
+echo "extra_cflags=\"/I$TRAVIS_WIN_DIR\\boringssl\"" >> out/Default/args.gn
+echo "extra_ldflags=\"$TRAVIS_WIN_DIR\\build\\ssl\\Release\\ssl.lib $TRAVIS_WIN_DIR\\build\\crypto\\Release\\crypto.lib\"" >> out/Default/args.gn
+echo "args.gn:"
+cat out/Default/args.gn
 
 cmd < $TRAVIS_BUILD_DIR/script-windows.bat
 
--- script-windows.bat
+++ script-windows.bat
@@ -3,6 +3,6 @@ set PATH=%TRAVIS_HOME%\depot_tools;c:\go\bin;%PATH%
 
 echo "---- NINJA CRASHPAD"
 cd %TRAVIS_BUILD_DIR%\crashpad\crashpad
-gn gen out\Default --args='target_cpu="x64" crashpad_use_boringssl_for_http_transport_socket=true'
+gn gen out\Default
 ninja -C out\Default
 
