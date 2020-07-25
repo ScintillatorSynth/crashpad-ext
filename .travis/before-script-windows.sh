7c278d594aa38fe2e0c0520f3b1cf5dbc46b93d9
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-25 12:12:00 -0700

    remove find command

--- .travis/before-script-windows.sh
+++ .travis/before-script-windows.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-echo "---- starting before-script"
 cmd < $TRAVIS_BUILD_DIR/before-script-windows.bat
-echo "---- before-script exit"
-
-find $TRAVIS_BUILD_DIR/boringssl -name "*.lib" -print
 
