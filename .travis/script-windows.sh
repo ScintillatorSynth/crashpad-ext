4ac950b4182a65ebfd2ace0145b10ff716dcef13
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-25 19:41:36 -0700

    change windows build to msvc dll

--- .travis/script-windows.sh
+++ .travis/script-windows.sh
@@ -4,8 +4,8 @@ export TRAVIS_WIN_DIR=`cygpath -w $TRAVIS_BUILD_DIR`
 cd $TRAVIS_BUILD_DIR/crashpad/crashpad
 mkdir -p out/Default
 echo "target_cpu=\"x64\"" > out/Default/args.gn
-echo "extra_cflags=\"/MT\"" >> out/Default/args.gn
-echo "extra_ldflags=\"/LTCG\"" >> out/Default/args.gn
+echo "extra_cflags=\"/MD\"" >> out/Default/args.gn
+# echo "extra_ldflags=\"/LTCG\"" >> out/Default/args.gn
 echo "args.gn:"
 cat out/Default/args.gn
 
