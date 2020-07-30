b915fe14ded1557165e781214ffb0ac7b3fc5a44
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-30 14:36:49 -0700

    fix target name for ossl build

--- .travis/before-install-linux.sh
+++ .travis/before-install-linux.sh
@@ -18,12 +18,12 @@ cd /usr/local/src
 sudo wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz
 sudo tar -xzf openssl-1.1.1g.tar.gz
 cd openssl-1.1.1g
-sudo ./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shard zlib
-sudo make
-sudo make test
-sudo make install
+sudo ./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared zlib || exit 1
+sudo make || exit 2
+sudo make test || exit 3
+sudo make install || exit 4
 sudo echo "/usr/local/ssl/lib" > /etc/ld.so.conf.d/openssl-1.1.1g.conf
-sudo ldconfig -v
+sudo ldconfig -v || exit 5
 
 # go 1.13 (prerequisite of BoringSSL, not available in apt)
 cd $TRAVIS_HOME
