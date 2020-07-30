0dac74590d856a6b7bbab597c59102fcd2d44960
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-30 15:28:45 -0700

    drop -L flags from boringssl includes

--- .travis/script-linux.sh
+++ .travis/script-linux.sh
@@ -8,7 +8,7 @@ cd $TRAVIS_BUILD_DIR/crashpad/crashpad
 mkdir -p out/Default
 echo "crashpad_use_boringssl_for_http_transport_socket=true" > out/Default/args.gn
 echo "extra_cflags=\"-I$TRAVIS_BUILD_DIR/boringssl -I/usr/local/ssl/include\"" >> out/Default/args.gn
-echo "extra_ldflags=\"-L$TRAVIS_BUILD_DIR/boringssl/build/ssl/libssl.a -L$TRAVIS_BUILD_DIR/boringssl/build/crypto/libcrypto.a\"" >> out/Default/args.gn
+echo "extra_ldflags=\"$TRAVIS_BUILD_DIR/boringssl/build/ssl/libssl.a $TRAVIS_BUILD_DIR/boringssl/build/crypto/libcrypto.a\"" >> out/Default/args.gn
 echo "args.gn:"
 cat out/Default/args.gn
 gn gen out/Default  || exit 1
