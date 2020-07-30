f22ebd846ec8fbf51634388c488eeb7c2b0edb4a
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-30 14:27:29 -0700

    try to build openssl1.1.1g from source

--- .travis/before-install-linux.sh
+++ .travis/before-install-linux.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install libssl-dev
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo update-alternatives --remove-all gcc
@@ -13,6 +11,19 @@ sudo update-alternatives --install /usr/bin/gcc gcc `which gcc-7` 10
 sudo update-alternatives --install /usr/bin/g++ g++ `which g++-7` 10
 sudo update-alternatives --set cc /usr/bin/gcc
 sudo update-alternatives --set c++ /usr/bin/g++
+
+# openssl 1.1.1 (needed to build crashpad)
+sudo apt-get install --yes checkinstall z1lib1g-dev
+cd /usr/local/src
+sudo wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz
+sudo tar -xzf openssl-1.1.1g.tar.gz
+cd openssl-1.1.1g
+sudo ./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shard zlib
+sudo make
+sudo make test
+sudo make install
+sudo echo "/usr/local/ssl/lib" > /etc/ld.so.conf.d/openssl-1.1.1g.conf
+sudo ldconfig -v
 
 # go 1.13 (prerequisite of BoringSSL, not available in apt)
 cd $TRAVIS_HOME
