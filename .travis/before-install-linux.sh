cd94458d8aa14055e485e405894eefb6e745b86f
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-30 22:50:36 -0700

    disable tests

--- .travis/before-install-linux.sh
+++ .travis/before-install-linux.sh
@@ -21,12 +21,12 @@ sudo tar -xzf openssl-1.1.1g.tar.gz
 cd openssl-1.1.1g
 sudo ./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl no-shared zlib -static || exit 1
 sudo make || exit 2
-sudo make test || exit 3
+# sudo make test || exit 3
 sudo make install || exit 4
 #sudo echo "/usr/local/ssl/lib" > /etc/ld.so.conf.d/openssl-1.1.1g.conf
 #sudo ldconfig -v || exit 5
-sudo rm -f /usr/local/ssl/lib/libssl*so*
-sudo rm -f /usr/local/ssl/lib/libcrypto*so*
+# sudo rm -f /usr/local/ssl/lib/libssl*so*
+# sudo rm -f /usr/local/ssl/lib/libcrypto*so*
 
 # go 1.13 (prerequisite of BoringSSL, not available in apt)
 cd $TRAVIS_HOME
